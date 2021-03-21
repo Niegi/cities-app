499245eb30da2e43fb778cef366bd3c70948a5d8
Niegowski Tomasz-PNM386 <Tomasz.Niegowski@motorolasolutions.com>
2021-03-21 13:43:02 +0100

    initial commit - adding project files

---
+++ src/test/java/pl/niegowski/citiesapp/CitiesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.niegowski.citiesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
